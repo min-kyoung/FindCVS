5c22514d37aabed159935604c2b0ca3afa2bfee8
min-kyoung <shalsrud9@naver.com>
2022-03-30 23:36:41 +0900

    Comment : DetailListBackgroundViewModel 주석 변경

--- FindCVS/Presentation/DetailListBackgroundViewModel.swift
+++ FindCVS/Presentation/DetailListBackgroundViewModel.swift
@@ -10,7 +10,7 @@ import RxCocoa
 
 struct DetailListBackgroundViewModel {
     // viewModel -> view
-    let isStatusLabelHidden: Signal<Bool>  // 만약 리스트에 가지고 온 데이터가 있다면 숨겨져야 하고, 아무런 데이터가 없다면 숨겨져야 한다.
+    let isStatusLabelHidden: Signal<Bool>  // 만약 리스트에 가지고 온 데이터가 있다면 숨겨져야 하고, 아무런 데이터가 없다면 보여져야 한다.
     
     // 외부에서 전달받을 값
     let shouldHideStatusLabel = PublishSubject<Bool>()
